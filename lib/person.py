5650906a3e62bd8b4c3cd86a87d9deddc7fa6e68
thelight0134 <amarhasan0134@gmail.com>
2023-06-01 13:46:36 +0800

    Initial commit

---
+++ lib/person.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, path, name):
+        self.path = path
+        self.name = name
+        self.img = []
+        self.img_tensor = []
+        self.img_face = []
+        self.img_emb = []
+        self.pca_x = []
+        self.pca_y = []
